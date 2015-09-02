a3289cd7e420e67659869d69bdcd08c4419b1908
Antonio Schmidt <antonio.schmidt@outlook.com>
2015-09-02 07:14:13 +0200

    template build: inject templates only to same component or shared view

--- gulp/build/pipes/html/templatePipe.js
+++ gulp/build/pipes/html/templatePipe.js
@@ -6,9 +6,17 @@ var inject = require('gulp-inject');
 var wrapper = require('gulp-wrapper');
 var rename = require('gulp-rename');
 
+function _transformTemplatePath(path) {
+    var isSharedPath = path.dirname === '.';
+
+    if (isSharedPath) {
+        path.dirname += '/' + config.templatesFolder;
+    }
+}
 
 function _templateSourcePipe() {
     return gulp.src(config.templateFiles)
+        .pipe(rename(_transformTemplatePath))
         .pipe(wrapper({
             header: '<script type="text/ng-template" id="${filename}">\n',
             footer: '</script>\n'
@@ -16,12 +24,24 @@ function _templateSourcePipe() {
 
 }
 
+function _createInjectedContent(filePath, file, index, length, targetFile) {
+    var isSourceAndTargetShared = targetFile.path.match(config.sharedViewFilesRegexp)
+            && filePath.match(config.sharedTemplateFilesRegexp),
+        componentName = isSourceAndTargetShared ? '' : config.componentNameFromViewFile(targetFile.path),
+        isSourceAndTargetSameComponent = !isSourceAndTargetShared
+            && targetFile.path.match(config.componentViewFilesRegexp)
+            && filePath.match(config.componentsTemplateFilesRegexp(componentName));
+
+    if (isSourceAndTargetShared || isSourceAndTargetSameComponent) {
+        return file.contents.toString('utf8');
+    }
+    return '';
+}
+
 function _templatePipe() {
     var injectOptions = {
         starttag: '<!-- inject:html -->',
-        transform: function (filePath, file) {
-            return file.contents.toString('utf8');
-        }
+        transform: _createInjectedContent
     };
 
     return lazyPipe()
--- gulp/config.js
+++ gulp/config.js
@@ -8,19 +8,41 @@ config.srcPath = 'src';
 config.distPath = 'dist';
 config.sharedFolder = 'shared';
 config.componentsFolder = 'components';
-config.viewFileSelector = 'views/**/*.html';
-config.templateFileSelector = 'templates/**/*.html';
+config.viewsFolder = 'views';
+config.templatesFolder = 'templates';
+config.viewFileSelector = config.viewsFolder + '/**/*.html';
+config.templateFileSelector = config.templatesFolder + '/**/*.html';
 
 config.sharedPath = path.join(config.srcPath, config.sharedFolder);
 config.sharedViewFiles = path.join(config.sharedPath, config.viewFileSelector);
+config.sharedViewFilesRegexp = /src\\shared\\views\\.*\.html/;
 config.sharedTemplateFiles = path.join(config.sharedPath, config.templateFileSelector);
+config.sharedTemplateFilesRegexp = /src\/shared\/templates\/.*\.html/;
 
 config.componentsPath = path.join(config.srcPath, config.componentsFolder);
 config.componentsSubFolderPath = path.join(config.componentsPath, '/**/');
 config.componentsViewFiles = path.join(config.componentsSubFolderPath, config.viewFileSelector);
+config.componentViewFilesRegexp = /src\\components\\.*\\views\\.*\.html/;
+config.componentFolderRegexp = /.*src\\components\\/;
+config.viewsFolderRegexp = /\\views.*/;
+config.componentNameFromViewFile = function (viewFile) {
+    return viewFile
+        .replace(config.componentFolderRegexp, '')
+        .replace(config.viewsFolderRegexp, '');
+};
+
 config.componentsTemplateFiles = path.join(config.componentsSubFolderPath, config.templateFileSelector);
+config.componentsTemplateFilesRegexp = function(component) {
+    return new RegExp("src\\/components\\/##COMPONENT##\\/templates\\/.*\\.html".replace("##COMPONENT##", component));
+};
 
 config.viewFiles = [config.sharedViewFiles, config.componentsViewFiles];
 config.templateFiles = [config.sharedTemplateFiles, config.componentsTemplateFiles];
+
+console.log('');
+console.log('***   CONFIG BEGIN ***');
+console.log(config);
+console.log('***   CONFIG END ***');
+console.log('');
 
 module.exports = config;
